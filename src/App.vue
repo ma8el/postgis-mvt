8ec87b301bab4a3a96a52ae8d2172ab758644afe
Markus Kurbel <m4rku@pm.me>
2023-03-18 13:18:32 +0100

    Add basic leaflet map

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Map from './components/Map.vue'
+components: {
+  Map
+}
+</script>
+
+<template>
+  <div class="mapContainer">
+    <Map />
+  </div>
+</template>
+
+<style>
+
+.mapContainer {
+  width: 100%;
+}
+
+</style>
---
+++ src/components/Map.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+onMounted(() => {
+  const map = L.map('map').setView([51.505, -0.09], 13);
+  L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+})
+</script>
+
+<template>
+  <div id="map"></div>
+</template>
+
+<style scoped>
+
+#map {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+}
+
+</style>
