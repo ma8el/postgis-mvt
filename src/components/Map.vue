d0498c5af6882cdde563ef5d8c50ea8a61c9732a
Markus Kurbel <m4rku@pm.me>
2023-03-27 19:31:14 +0200

    Add basic attribute interactions

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -3,9 +3,9 @@
   import 'ol/ol.css';
   import Map from 'ol/Map';
   import View from 'ol/View';
-  import TileLayer from 'ol/layer/Tile';
-  import OSM from 'ol/source/OSM';
   import MVT from 'ol/format/MVT';
+  import OSM from 'ol/source/OSM';
+  import TileLayer from 'ol/layer/Tile';
   import VectorTileLayer from 'ol/layer/VectorTile';
   import VectorTileSource from 'ol/source/VectorTile';
   import {Stroke, Style, Fill} from 'ol/style.js';
@@ -21,25 +21,44 @@
   const port = ref(props.port);
   const table = ref(props.table);
 
+  const accessToken = import.meta.env.VITE_MAPBOX_TOKEN
+
   onMounted(() => {
     var vtLayer = new VectorTileLayer({
       declutter: false,
       source: new VectorTileSource({
         format: new MVT(),
-        url: `http://${host.value}:${port.value}/mvt/${table.value}/{z}/{x}/{y}.mvt`,
+        url: `http://${host.value}:${port.value}/api/v1/mvt/${table.value}/{z}/{x}/{y}.mvt`,
       }),
       renderMode: 'vector',
-      style: new Style({
+      style: (feature) => {
+        const building = feature.get('building');
+        let color = 'rgba(176, 196, 222, 0.4)';
+        let width = 1;
+        let strokeColor = 'blue';
+
+        if (building === 'industrial') {
+          color = 'rgba(255, 192, 192, 0.4)';
+          width = 1;
+          strokeColor = 'red';
+        }
+        else if (building === 'commercial' || building === 'retail') {
+          color = 'rgba(192, 255, 192, 0.4)';
+          width = 1;
+          strokeColor = 'green';
+        }
+        return new Style({
           stroke: new Stroke({
-            color: 'blue',
-            width: 1
+            color: strokeColor,
+            width: width
           }),
-      fill: new Fill({
-        color: 'rgba(176, 196, 222, 0.4)'
-      })
-      })
+          fill: new Fill({
+            color: color
+          })
+        });
+      }
     });
-  
+
     const map = new Map({
       target: 'map',
       layers: [
@@ -52,7 +71,13 @@
         center: fromLonLat([8.6821, 50.1109]),
         zoom: 13,
       }),
-    })
+    });
+
+    map.on('click', function(evt) {
+      map.forEachFeatureAtPixel(evt.pixel, function(feature, layer) {
+        console.log(feature.getProperties());
+      });
+    });
   });
 </script>
 
